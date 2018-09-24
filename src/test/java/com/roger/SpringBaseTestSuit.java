2bbb7c7eb0f20e87fdb86453f38ae54fef8817e6
LiHongTai <631396529@qq.com>
2018-09-24 16:43:54 +0800

    commit redis sentinel

--- src/test/java/com/roger/SpringBaseTestSuit.java
+++ src/test/java/com/roger/SpringBaseTestSuit.java
@@ -1,12 +1,12 @@
 package com.roger;
 
-import com.roger.config.RedisClusterConfig;
+import com.roger.config.RedisSentinelConfig;
 import com.roger.config.SpringBeanConfig;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {SpringBeanConfig.class,RedisClusterConfig.class})
+@ContextConfiguration(classes = {SpringBeanConfig.class, RedisSentinelConfig.class})
 public class SpringBaseTestSuit {
 }
